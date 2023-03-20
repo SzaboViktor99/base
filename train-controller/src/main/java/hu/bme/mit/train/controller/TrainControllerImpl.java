17ca9296ba15232893e9a2da9a35e26874d95e52
En <you@example.com>
2023-03-20 17:39:07 +0100

    fix compile error

--- train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
+++ train-controller/src/main/java/hu/bme/mit/train/controller/TrainControllerImpl.java
@@ -25,7 +25,6 @@ public class TrainControllerImpl implements TrainController {
 
 	public int speedCorrection(int actualSpeed, int offset){
 		//Adds an offset to the actual speed.
-		randomforditasihiba
 		int correctedSpeed = actualSpeed+offset;
 		return correctedSpeed;
 	}
