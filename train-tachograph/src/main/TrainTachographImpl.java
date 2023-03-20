ce2c783bdfba1743a830b82d109cfb7d5dc2f3cb
En <you@example.com>
2023-03-20 18:28:36 +0100

    Tachograph implemented

---
+++ train-tachograph/src/main/TrainTachographImpl.java
@@ -0,0 +1,27 @@
+import hu.bme.mit.train.interfaces.TrainUser;
+import hu.bme.mit.train.interfaces.TrainController;
+
+import com.google.common.collect.TreeBasedTable;
+import com.google.common.collect.Table;
+import java.util.Date;
+
+
+public class TrainTachographImpl{
+    private TrainUser user;
+    private TrainController controller;
+    private Table<Integer, Integer, Date> tachoData;
+
+    public TrainTachographImpl(TrainUser user, TrainController controller){
+        this.user = user;
+        this.controller = controller;
+        tachoData = new TreeBasedTable.create();
+    }
+
+    public void logTachoData(){
+        tachoData.put(
+            user.getJoystickPosition(),
+            controller.getReferenceSpeed(),
+            new Date()
+        );
+    }
+}
